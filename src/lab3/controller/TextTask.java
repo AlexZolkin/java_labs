377c45221a5b1020e0e7b626b87a3b8672595204
alexzolkin19 <alexzolkin19@gmail.com>
2017-04-11 01:52:04 +0300

    implementation of ITextTask, never used in strait because of factory method in ITextTask

---
+++ src/lab3/controller/TextTask.java
@@ -0,0 +1,34 @@
+package lab3.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Алексей on 11.04.2017.
+ */
+public class TextTask implements ITextTask{
+    @Override
+    public StringBuffer textReplace(StringBuffer text) {
+        StringBuffer result = new StringBuffer();
+        List<String> sentences = Arrays.asList(text.toString().split("\\."));
+        for(String sentence : sentences){
+            //List<String> words = Arrays.asList(sentence.split(" "));
+            List<String> words = new ArrayList<>(Arrays.asList(sentence.split(" ")));
+            if(words.get(0).equals(""))
+                words.remove(0);
+            if(words.get(words.size() - 1).equals(""))
+                words.remove(words.size() - 1);
+            if(words.size() > 1)
+            swap(words);
+            result.append(String.join(" ", words.toArray(new String[0])));
+            result.append(". ");
+        }
+        return result;
+    }
+    private void swap(List<String> words){
+        String tmp = words.get(0);
+        words.set(0, words.get(words.size() - 1));
+        words.set(words.size() - 1, tmp);
+    }
+}
