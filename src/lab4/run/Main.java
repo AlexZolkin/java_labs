b647d8d8f814020683f797657083732142f2a88d
alexzolkin19 <alexzolkin19@gmail.com>
2017-04-11 19:19:31 +0300

    Main class. Used for input and output

---
+++ src/lab4/run/Main.java
@@ -0,0 +1,14 @@
+package lab4.run;
+
+import lab4.controller.ITextTask;
+
+/**
+ * Created by Алексей on 11.04.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        StringBuffer text = new StringBuffer(lab3.run.Main.readText());
+        System.out.print("Text with first and last words in sentences replaced: ");
+        System.out.println(ITextTask.create().textReplace(text));
+    }
+}
