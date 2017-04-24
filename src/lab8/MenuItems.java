825e1cf23320192c12e8393402eaa42ee3e815c8
alexzolkin19 <alexzolkin19@gmail.com>
2017-04-24 03:34:32 +0300

    just enum to make console menu realisation more readable

---
+++ src/lab8/MenuItems.java
@@ -0,0 +1,13 @@
+package lab8;
+
+/**
+ * Created by Алексей on 24.04.2017.
+ */
+public enum MenuItems {
+    READ_STRINGS,
+    WRITE_STRINGS,
+    READ_COLLECTION,
+    WRITE_COLLECTION,
+    READ_OBJECTS_ARRAY,
+    WRITE_OBJECTS_ARRAY
+}
